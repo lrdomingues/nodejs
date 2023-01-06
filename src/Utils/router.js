0c1f3e7230ebb11dfaa1aef0e513325d78ce7645
lrdomingues <51265303+lrdomingues@users.noreply.github.com>
2023-01-06 03:54:37 -0300

    Update router.js

--- src/Utils/router.js
+++ src/Utils/router.js
@@ -28,6 +28,8 @@ router.delete('/comments/:comment_id/', AuthMiddleware, CommentController.delete
 router.put('/comments/:comment_id', AuthMiddleware, CommentController.editComment)
 router.get('/report', AuthMiddleware, ReportController.getReport)
 
-router.get('/hello-world', (req,res){ return res.status(200).send({message: "Hello World!"})})
+app.get('/hello', (req, res) => {
+  res.send('Hello World!')
+})
 
 module.exports = router
