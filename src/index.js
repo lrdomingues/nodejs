8ff6f8ab9e2834d8d5c1d70a793f9cc113962373
lrdomingues <51265303+lrdomingues@users.noreply.github.com>
2023-01-06 03:30:22 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,5 +17,5 @@ app.use(cors())
 app.use(express.json())
 app.use(router)
 
-app.listen(process.env.PORT || 3333, () => console.log('Server running'))
+app.listen(process.env.PORT, () => console.log('Server running'))
 
